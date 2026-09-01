87928e2cd1cccbd127cf22d34d232ba497f7a273
Tomás <77382063+tvarnetperez@users.noreply.github.com>
2026-08-31 22:36:42 -0400

    Create addins.R

---
+++ addins.R
@@ -0,0 +1,7 @@
+# Hotkey to align assignment operators
+remotes::install_github("seasmith/AlignAssign")
+
+# To fiddle vectors, auto-toggle quotes and facilitate pasting from the web
+install.packages(
+   "datapasta",
+   repos = c(mm = "https://milesmcbain.r-universe.dev", getOption("repos")))
